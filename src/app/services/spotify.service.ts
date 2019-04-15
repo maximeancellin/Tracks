85e34e6c8c45a67e22ce9d299032a676cf80802f
Maxime Ancellin <maxime.ancellin@gmail.com>
2019-04-15 23:20:37 +0200

    create all request for manage playlist (wait testing)

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {BehaviorSubject, Observable, of} from 'rxjs';
 import {catchError, map, tap} from 'rxjs/operators';
 import {environment} from '../../environments/environment';
+import {any} from 'codelyzer/util/function';
 
 @Injectable({
   providedIn: 'root'
@@ -50,18 +51,198 @@ export class SpotifyService {
 
   public search(input, type = 'track,artist', market = 'FR', limit = '20', offset = '0'): Observable<{}> {
     const head = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+
+    const parameters = this.toQueryString({
+      'q': input,
+      'type': type,
+      'market': market,
+      'limit': limit,
+      'offset': offset
+    });
+
+    return this.http.get(this.apiUrl + 'search?' + parameters, {headers: head}).pipe(
+      tap((data: {}) => {
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  /*
+   *  END Search
+   */
+
+  /*
+   *  START Playlist
+   */
+
+  public playlistDeleteTracks(id, trackId, trackPos): Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+
+    const bodyRequest = {
+      tracks: [{
+        uri: trackId,
+        positions: [trackPos]
+      }
+      ]
+    };
+
+    return this.http.request('delete', this.apiUrl + 'playlists/' + id + '/tracks', {body: bodyRequest}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playlists(limit = 20, offset = 0): Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+
+    const parameters = this.toQueryString({
+      'limit': limit,
+      'offset': offset
+    });
+
+
+    return this.http.get(this.apiUrl + 'playlists?' + parameters, {headers: head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playlistsById(id, market = 'fr'/*, fields*/): Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+
+    const parameters = this.toQueryString({
+      'market': market// ,
+      /*'fields': fields*/
+    });
+
+
+    return this.http.get(this.apiUrl + 'playlists/' + id + '?' + parameters, {headers: head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playlistsByUser(id, limit = 20, offset = 0): Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+
+    const parameters = this.toQueryString({
+      'limit': limit,
+      'offset': offset
+    });
+
+
+    return this.http.get(this.apiUrl + 'users/' + id + '/playlists?' + parameters, {headers: head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playlistTracks(id, market, limit = 20, offset = 0, field = 'items(added_by.id,track(name,href,album(name,href)))')
+    : Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+
+    const parameters = this.toQueryString({
+      'market': market,
+      'limit': limit,
+      'offset': offset,
+      'fields': field
+    });
+
+
+    return this.http.get(this.apiUrl + 'playlists/' + id + '/tracks?' + parameters, {headers: head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playlistAddTracks(id, position, uris): Observable<{}> {
+    const head = new HttpHeaders({
       'Content-Type' : 'application/json; charset=utf-8'
     });
 
     const parameters = this.toQueryString({
-      'q' : input,
-      'type' : type,
-      'market' : market,
-      'limit' : limit,
-      'offset' : offset
+      'position': position,
+      'uris': uris
     });
 
-    return this.http.get(this.apiUrl + 'search?' + parameters, {headers : head}).pipe(
+
+    return this.http.post(this.apiUrl + 'playlists/' + id + '/tracks?' + parameters, any, {headers: head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playlistCreate(name, description = '', type = false): Observable<{}> {
+    const bodyData = {
+      name: name,
+      description: description,
+      public: type
+    };
+
+    return this.http.post(this.apiUrl + 'playlists', bodyData).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playlistUpdate(id, name, description = '', type = false): Observable<{}> {
+    const bodyData = {
+      name: name,
+      description: description,
+      public: type
+    };
+
+    return this.http.post(this.apiUrl + 'playlists/' + id, bodyData).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playlistReorder(id, start, length, before): Observable<{}> {
+    const bodyData = {
+      range_start: start,
+      range_length: length,
+      insert_before: before
+    };
+
+    return this.http.post(this.apiUrl + 'playlists/' + id + '/tracks', bodyData).pipe(
       tap((data: {}) => {
         console.log(data);
         this.data$.next(this.data);
@@ -71,7 +252,7 @@ export class SpotifyService {
   }
 
   /*
-   *  END Search
+   *  END Playlist
    */
 
   /*
