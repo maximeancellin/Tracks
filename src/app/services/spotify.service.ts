057378721563cf2013cfa70cafe6ac695f80ed96
Maxime Ancellin <maxime.ancellin@gmail.com>
2019-04-19 11:46:55 +0200

    players implementation -> done TODO try

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {BehaviorSubject, Observable, of} from 'rxjs';
 import {catchError, map, tap} from 'rxjs/operators';
 import {environment} from '../../environments/environment';
+import {any} from 'codelyzer/util/function';
 
 @Injectable({
   providedIn: 'root'
@@ -72,6 +73,198 @@ export class SpotifyService {
 
   /*
    *  END Search
+   */
+
+  /*
+   *  START Player
+   */
+
+  public playerRecent(limit = 10): Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type' : 'application/json; charset=utf-8'
+    });
+
+    const parameters = this.toQueryString({
+      'limit' : limit,
+    });
+
+    return this.http.get(this.apiUrl + 'me/player/recently-played?' + parameters, {headers : head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerNow(): Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type' : 'application/json; charset=utf-8'
+    });
+
+    return this.http.get(this.apiUrl + 'me/player', {headers : head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerDevice(): Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type' : 'application/json; charset=utf-8'
+    });
+
+    return this.http.get(this.apiUrl + 'me/player/devices', {headers : head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerTrackNow(): Observable<{}> {
+    const head = new HttpHeaders({
+      'Content-Type' : 'application/json; charset=utf-8'
+    });
+
+    return this.http.get(this.apiUrl + 'me/player/currently-playing', {headers : head}).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerNext(device): Observable<{}> {
+    const parameters = this.toQueryString({
+      'device_id' : device,
+    });
+
+    return this.http.post(this.apiUrl + 'me/player/next?' + parameters, any).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerPrevious(device): Observable<{}> {
+    const parameters = this.toQueryString({
+      'device_id' : device,
+    });
+
+    return this.http.post(this.apiUrl + 'me/player/previous?' + parameters, any).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerPause(device): Observable<{}> {
+    const parameters = this.toQueryString({
+      'device_id' : device,
+    });
+
+    return this.http.put(this.apiUrl + 'me/player/pause?' + parameters, any).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerRepeat(device, state = 'context'): Observable<{}> {
+    const parameters = this.toQueryString({
+      'device_id' : device,
+      'state' : state,
+    });
+
+    return this.http.put(this.apiUrl + 'me/player/repeat?' + parameters, any).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerSeek(device, time = 2500): Observable<{}> {
+    const parameters = this.toQueryString({
+      'device_id' : device,
+      'position_ms' : time,
+    });
+
+    return this.http.put(this.apiUrl + 'me/player/seek?' + parameters, any).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerShuffle(device, state = true): Observable<{}> {
+    const parameters = this.toQueryString({
+      'device_id' : device,
+      'state' : state,
+    });
+
+    return this.http.put(this.apiUrl + 'me/player/shuffle?' + parameters, any).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerVolume(device, volume = 100): Observable<{}> {
+    const parameters = this.toQueryString({
+      'device_id' : device,
+      'volume_percent' : volume,
+    });
+
+    return this.http.put(this.apiUrl + 'me/player/volume?' + parameters, any).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  public playerPlay(device, uri, offset = 0, time = 0): Observable<{}> {
+    const parameters = this.toQueryString({
+      'device_id' : device,
+    });
+
+    const requestBody = {
+      context_uri : uri,
+      offset : {
+        position : offset
+      },
+      position_ms : time
+    };
+
+    return this.http.put(this.apiUrl + 'me/player/play?' + parameters, requestBody).pipe(
+      tap((data: {}) => {
+        console.log(data);
+        this.data$.next(this.data);
+      }),
+      catchError(this.handleError('getSelf'))
+    );
+  }
+
+  /*
+   *  END Player
    */
 
   /*
