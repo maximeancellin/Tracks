9bbb3858adf8de2bcc001ce9384ca1133577101f
Maxime Ancellin <maxime.ancellin@gmail.com>
2019-05-04 14:57:43 +0200

    get user data in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {CheckAuthService} from '../../services/check-auth.service';
+import {Subscription} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
+import {SpotifyService} from '../../services/spotify.service';
+import {TokenService} from 'spotify-auth';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +11,17 @@ import {CheckAuthService} from '../../services/check-auth.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private stream: Subscription | null = null;
 
-  constructor(private auth: CheckAuthService) {
+  constructor(private auth: CheckAuthService, private tokenSvc: TokenService, private spotify: SpotifyService) {
     // auth.checkConnection();
   }
 
   ngOnInit() {
+    const stream = this.tokenSvc.authTokens.pipe(switchMap((x) => {
+      return this.spotify.userInfo();
+    }));
+    this.stream = stream.subscribe((x) => this.user = x);
   }
 
 }
